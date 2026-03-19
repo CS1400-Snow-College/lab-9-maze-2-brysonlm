d26fd68cf060641e9a0ad4896552f5cba6e6c5e4
Bryson <bryson.leon-menjivar@students.snow.edu>
2026-03-18 23:48:32 -0600

    Import the map.txt into the terminal

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Bryson Leon, 3/18/26, Maze 2
+
+Console.WriteLine("Welcome to the Maze Game!");
+Console.WriteLine("Use the arrow keys to move collect ^, avoid %, find # to win.");
+Console.WriteLine("Press any key to start...");
+Console.ReadKey();
+Console.Clear();
+
+string[] mapRows = File.ReadAllLines("map.txt");
+
+for (int i = 0; i < mapRows.Length; i++)
+{
+    Console.WriteLine(mapRows[i]);
+}
