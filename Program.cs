c275a4ed22a724b42048d24d941fd5528b06488d
Bryson <bryson.leon-menjivar@students.snow.edu>
2026-03-18 23:52:22 -0600

    movement added

--- Program.cs
+++ Program.cs
@@ -12,3 +12,33 @@ for (int i = 0; i < mapRows.Length; i++)
 {
     Console.WriteLine(mapRows[i]);
 }
+
+int cursorTop = 1;
+int cursorLeft = 1;
+Console.SetCursorPosition(cursorLeft, cursorTop);
+
+ConsoleKey input;
+do
+{
+    input = Console.ReadKey(true).Key;
+
+    int proposedTop = cursorTop;
+    int proposedLeft = cursorLeft;
+
+    switch (input)
+    {
+        case ConsoleKey.UpArrow: proposedTop--; break;
+        case ConsoleKey.DownArrow: proposedTop++; break;
+        case ConsoleKey.LeftArrow: proposedLeft--; break;
+        case ConsoleKey.RightArrow: proposedLeft++; break;
+    }
+
+    // Enforce Boundaries
+    if (proposedTop >= 0 && proposedTop < mapRows.Length && 
+        proposedLeft >= 0 && proposedLeft < mapRows[proposedTop].Length)
+    {
+        cursorTop = proposedTop;
+        cursorLeft = proposedLeft;
+        Console.SetCursorPosition(cursorLeft, cursorTop);
+    }
+} while (input != ConsoleKey.Escape);
--- obj/Debug/net10.0/lab-9-maze-2-brysonlm.AssemblyInfo.cs
+++ obj/Debug/net10.0/lab-9-maze-2-brysonlm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-9-maze-2-brysonlm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c47849ea3887a9aa507931dde3c37d9bf83260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26fd68cf060641e9a0ad4896552f5cba6e6c5e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-9-maze-2-brysonlm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-9-maze-2-brysonlm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
